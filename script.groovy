74c1f5f1eb97bf53da9e8065ab78cacbda65bf87
AhmedKhamis <akhamis574@gmail.com>
2024-08-09 21:56:40 +0300

    Add shared library

--- script.groovy
+++ script.groovy
@@ -1,16 +1,16 @@
-def buildJar() {
-    echo 'Building the application...'
-    sh 'mvn package'
-}
+// def buildJar() {
+//     echo 'Building the application...'
+//     sh 'mvn package'
+// }
 
-def buildImage() {
-    echo 'Building the docker image...'
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-depi', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-            sh 'docker build -t ahmed1612/my-app:jmv-1.0 .'
-            sh "echo $PASS | docker login -u $USER --password-stdin"
-            sh 'docker push ahmed1612/my-app:jmv-1.0'
-    }
-}
+// def buildImage() {
+//     echo 'Building the docker image...'
+//     withCredentials([usernamePassword(credentialsId: 'dockerhub-depi', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+//             sh 'docker build -t ahmed1612/my-app:jmv-1.0 .'
+//             sh "echo $PASS | docker login -u $USER --password-stdin"
+//             sh 'docker push ahmed1612/my-app:jmv-1.0'
+//     }
+// }
 
 def deployApp() {
     echo 'deploying the application...'
